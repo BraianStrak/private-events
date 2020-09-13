680d19618e939dbd9a524be44c39a69a13447a4c
BraianStrak <braianstrak1999@gmail.com>
2020-09-13 20:35:58 +0100

    Created event model and migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        #the below worked, meaning this controller is actually connected to the user. good!
+        redirect_to root_path
     end
 
     def show
