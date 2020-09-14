2ba137a98881878ae40c22b6134ae3436d532105
BraianStrak <braianstrak1999@gmail.com>
2020-09-14 20:56:02 +0100

    Added some views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,9 +6,23 @@ class EventsController < ApplicationController
 
     def index
         #a view of all events 
+        @events = Event.all
     end
 
     def create
         #redirect to event index or the user created events, idk. 
+        @event = current_user.created_events.build(event_params)
+        if @event.save
+          #current_user.attended_events << @event
+          redirect_to root_path #redirects to the events created by the user ONLY
+        else
+          flash.now[:danger] = @event.errors.full_messages
+          render :new
+        end
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:title, :body, :date)
     end
 end
